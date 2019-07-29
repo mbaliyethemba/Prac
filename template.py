57afa59431498483a1ffdc0eb0583545abdd06e6
SHNMBA004 <SHNMBA004@myuct.ac.za>
2019-07-29 22:04:00 +0100

    Added the button handler

--- template.py
+++ template.py
@@ -16,8 +16,8 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 
 #set the buttons on pin 17 and 27
-GPIO.setup(17,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(27,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(17,GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #incremental button
+GPIO.setup(27,GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #decremental button
 
 #set the LED
 GPIO.setup(24,GPIO.OUT)  #LED3
@@ -30,6 +30,26 @@ GPIO.setup(23,GPIO.LOW)  #LED2
 GPIO.setup(22,GPIO.LOW)  #LED1
 
 counter = 0;
+
+def button():
+	global counter
+	if (GPIO.input(17) == 1 and counter != 7): #counts up
+		counter += 1
+		print bin(counter)[2:].zfill(3)
+		time.sleep(.2)
+	if (GPIO.input(17) == 1 and counter == 7): #wraps around to zero
+		counter = 0
+		print bin(counter)[2:].zfill(3)
+		time.sleep(.2)
+	if (GPIO.input(27) == 1 and counter != 0): #counts down
+		counter -= 1
+		print bin(counter)[2:].zfill(3)
+		time.sleep(.2)
+	if (GPIO.input(27) == 1 and counter == 0): #wraps around back to 7
+		counter = 7
+		print bin(counter)[2:].zfill(3)
+		time.sleep(.2)
+	return
 
 # Logic that you write
 def main():
