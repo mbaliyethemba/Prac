1a8d7f5687071d6ce80e592b089c293101452fa5
SHNMBA004 <SHNMBA004@myuct.ac.za>
2019-07-29 22:57:30 +0100

    Restructured the print statements

--- template.py
+++ template.py
@@ -36,19 +36,19 @@ def button():
 	global counter
 	if (GPIO.input(11) == 1 and counter != 7): #counts up
 		counter += 1
-		print bin(counter)[2:].zfill(3)
+		print (bin(counter)[2:].zfill(3))
 		time.sleep(.2)
 	if (GPIO.input(11) == 1 and counter == 7): #wraps around to zero
 		counter = 0
-		print bin(counter)[2:].zfill(3)
+		print (bin(counter)[2:].zfill(3))
 		time.sleep(.2)
 	if (GPIO.input(13) == 1 and counter != 0): #counts down
 		counter -= 1
-		print bin(counter)[2:].zfill(3)
+		print (bin(counter)[2:].zfill(3))
 		time.sleep(.2)
 	if (GPIO.input(13) == 1 and counter == 0): #wraps around back to 7
 		counter = 7
-		print bin(counter)[2:].zfill(3)
+		print (bin(counter)[2:].zfill(3))
 		time.sleep(.2)
 	return
 
